e0b3b80a86f0ef95fb86f2847092b29eedb44ff0
Glory Olusola <olusolagloryolamide@gmail.com>
2021-08-02 11:16:12 +0100

    feat: added features of getting exchange rates (#9)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const BASE = require("./lib/paycoins.base"); 
 const invoice = require("./lib/paycoins.invoice");
+const exchangeRates = require("./lib/paycoins.exchangeRates");
 
 function Paycoins (public_key, secret_key) {
     const paycoinsbase = new BASE(
@@ -8,6 +9,7 @@ function Paycoins (public_key, secret_key) {
     );
 
     this.Invoice = new invoice(paycoinsbase);
+    this.ExchangeRates = new exchangeRates(paycoinsbase);
 };
 
 module.exports = Paycoins;
